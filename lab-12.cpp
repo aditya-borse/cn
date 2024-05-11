caef81346c54dbdc24ebc3e4e7158f97ae3a33ba
Aditya Borase <109383195+aditya-borse@users.noreply.github.com>
2024-05-11 10:54:08 +0530

    Update lab-12.cpp

--- lab-12.cpp
+++ lab-12.cpp
@@ -1,17 +1,4 @@
 // Write a program for error detection and correction for 7/8 bits ASCII codes
 // using Hamming Codes or CRC
 
-#include <bits/stdc++.h>
-using namespace std;
-
-vector<int> encode(vector<int> data)
-{
-    int n = data.size();
-    int k = n + 4;
-    vector<int> code(k, 0);
-    
-}
-
-int main() {
-    vector <int> data = {}
-}
+// To be completed 
